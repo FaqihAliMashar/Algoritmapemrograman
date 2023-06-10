3a80f8d6536c4295bedd258b0c98cf1860de8263
FaqihAliMashar <136144674+FaqihAliMashar@users.noreply.github.com>
2023-06-10 12:56:11 +0700

    Update Algo_Latihan1.py
    Menambahkan source code tentang penilaian KHS Mahasiswa

--- Algo_Latihan1.py
+++ Algo_Latihan1.py
@@ -1,1 +1,36 @@
-maaf belum diisi, nantikan acara selanjutnya
+
+#Setting Variabel
+nim = "20122060"
+nama ="Faqih"
+kelas ="2A"
+prodi= "Informatika"
+
+#Setting Variabel nilai
+Bhs_indo =76
+Bhs_ing  =70
+Pd       =90
+Mtk      =65
+Kal1     =67
+So       =75
+
+#Perhitungan
+
+rata =(Bhs_Indo+Bhs_Ing+Pd+Mtk+Kal1+So)/6
+#Cetak Hasil
+print ("Nim Anda          :",nim)
+print ("Nama lengkap anda :",nama)
+print ("kelas             :",kelas)
+print ("Program Studi     :",prodi)
+print ("--------------------------")
+print ("Hasil Kartu Hasil Studi")
+print("---------------------------")
+print ("1.Bahasa Indonesia ",bhs_indo)
+print ("2.Bahasa Inggris",bhs_Inggris)
+print ("3.Pemrograman Dasar", pd)
+print ("4.Matematika",mtk)
+print ("5.Kalkulus 1",kal1)
+print ("6.Sistem Operasi",so) 
+print ("-------------------------")
+print ("Rata-rata" ,rata)
+print ("-------------------------")
+
