97aef274f4daf7e9dbc33cd2868ce300f217dbbd
FaqihAliMashar <136144674+FaqihAliMashar@users.noreply.github.com>
2023-06-15 13:23:10 +0700

    Update Algo_Latihan 2.py
    HasilOperasiMatematikaAlgoLatihan2

--- Algo_Latihan 2.py
+++ Algo_Latihan 2.py
@@ -1,1 +1,31 @@
-maaf belum diisi
+#setting variabel
+a=10
+b=20
+c=50
+
+menghitung 1A(a , b, c) :
+  result=((a+b)*(b*c))/(a+b+c) 
+  return result
+
+menghitung 2B(a, b, c) :
+  p=a
+  l=b
+  t=c
+  result=p*l*t
+  return result
+
+
+menghitung 3C(a, c):
+  t=c
+  result=0.5*a*c
+  return result
+
+
+mengehitung 4D(a, b, c, d) :
+  result=(((a+b) *2) +((b*c) *2))/(a*b) 
+  return result
+
+print(f"Hasil A={menghitung A(a, b, c) }") 
+print(f"Hasil B={menghitung B(a, b, c) }") 
+print(f"Hasil C={menghitung C(a, c) }") 
+print(f"Hasil D={menghitung D(a, b, c)}") 
