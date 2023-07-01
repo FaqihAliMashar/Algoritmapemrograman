fd62e2f430cb690113d0f274b56eb45f6bdd0f0a
FaqihAliMashar <136144674+FaqihAliMashar@users.noreply.github.com>
2023-07-01 20:53:28 +0700

    Update latihan6_tipedatalanjutan.py

--- latihan6_tipedatalanjutan.py
+++ latihan6_tipedatalanjutan.py
@@ -1,1 +1,53 @@
+#setting variabel identitas 
+nim   = input("Masukan NIM     : ")
+nama  = input("Masukan Nama    : ")
+kelas = input("Masukan Kelas   : ")
+prodi = input("Masukan Jurusan : ")
 
+name = input("Masukan Nama Bangunan : ")
+r = input("Jari-Jari : ")
+phi = input("masukkan phi:")
+
+#operasi
+luas=3.14*7*7
+
+#tapilan A
+print ("A")
+print ("================================")
+print ("      OPERASI ARITMAITKA        ")
+print ("================================")
+print ("")
+print ("Nim anda          :%s"%(nim))
+print ("Nama lengkap anda :%s"%(nama))
+print ("Kelas anda        :%s"%(kelas))
+print ("Progam Studi anda :%s"%(prodi))
+print ("")
+print ("--------------------------------")
+print ("   Bangunan %s"%(nama))
+print ("--------------------------------")
+print ("")
+print ("nilai Jari-Jari :%s"%(r))
+print ("luas Bangunan   :%d"%(luas))
+print ("")
+print ("================================")
+
+#list
+ali= ["UNISS","di","belajar","pada","Mahasiswa","Pemrograan","ruang","lab","Algoritma","semester","topik","Data","Tipe","dan","dengan","Batang",2]
+#       0      1      2        3        4           5           6     7      8            9          10    11     12     13     14       15    16
+
+#tampilan B
+print ("B")
+print ("=======================================================")
+print ("                    SUSUN KATA SPOK         ")
+print ("=========================================================")
+print ("")
+print ("SPOK")
+print ("")
+print ("=================================================")
+
+print("----------------------------------------------------------")
+print("susun kata spok...")
+print("----------------------------------------------------------")
+kata=["mahasiswa","UNISS","Batang","semester",2,"belajar","topik","algoritma","pemrograman","dan","tipe","data","di","lab"] 
+print ("menyusun kata ")
+print(kata[0],kata[1],kata[2],kata[3],kata[4],kata[5],kata[6],kata[7],kata[8],kata[9],kata[10],kata[11],kata[12],kata[13])
